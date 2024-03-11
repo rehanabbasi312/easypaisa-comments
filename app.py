d5d10ab02847e08644b6560b5d85b132b4617b26
unknown <rehan.naveed@zigron.com>
2024-03-11 11:22:23 +0500

    Easypaisa Bot 2.0 with UI on 10K Param

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from response import getResponse
 
+app = Flask(__name__, static_url_path='', static_folder='static', template_folder='templates')
 
-app = Flask(__name__)
-
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/api', methods=['POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        name = request.form['name']
-        rating = int(request.form['rating'])
-        feedback = request.form['feedback']
+    return render_template('index.html')
 
+@app.route('/api', methods=['POST'])
+def api():
+    try:
+        data = request.get_json()
+        name = data['name']
+        rating = data['rating']
+        feedback = data['comment']
+
+        # Call your Python script function
         response = getResponse(name, rating, feedback)
 
-        return render_template('index.html', name=name, rating=rating, feedback=feedback, response=response)
+        return jsonify({'response': response})
 
-    return render_template('index.html')
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
-
