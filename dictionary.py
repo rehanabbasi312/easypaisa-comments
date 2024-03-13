3c5d814ee259bd0898382c7061f26fc442040802
unknown <rehan.naveed@zigron.com>
2024-03-13 09:17:13 +0500

    New Parameter for heuristics are added

---
+++ dictionary.py
@@ -0,0 +1,46 @@
+negativeWordsInRoman = ["ghatya","ghatia","baikar","not good","buri","ajeeb","garbage","useless","crap","horrible",
+                        "disappointing","frustrating","frustrated","trash","terrible","awful","unusable","bekaar","worthless",
+                        "mayoos kun","mayoos","failure","failed","bad","bura","ineffective","disliked","dislike", "chawal", "sust","sai nai",
+                        "sahi nai", "nai sahi", "nai sai", "nend","not helpful"]
+
+greetingsList = ["Hey", "Dear","Hello", "Hi"]
+
+highRating = ["""Thank you for providing us with a high rating. We apologize for any inconvenience and strive to make 
+your experience smoother. 💚 Please share the details of your issue with us through our inbox, and our team will 
+promptly address and resolve it for you. https://www.facebook.com/easypaisa/ 😊""","""Thank you for taking the time to rate us highly.🌟 We 
+truly appreciate your feedback, as it helps us identify areas for improvement and ensures we continue to deliver the best possible experience to
+all our users."""]
+
+lowRating = ["""We're delighted that you shared your positive experience. Please consider giving us a higher star rating. Keep using easypaisa! 
+             ✨ For any info & support, feel free to reach out to us on Whatsapp: https://wa.me/923411103737?text=Hi 💚""", """Thank you for sharing your positive experience with us! 💚
+             Please consider giving us a higher star rating. Keep using easypaisa! ✨ For any info & support, feel free to reach out to us on Whatsapp: https://wa.me/923411103737?text=Hi"""]
+
+apologiseComments = ["""We are sorry for the inconvenience and would like to make your experience easy. 💚 Kindly share the details of your 
+                     issue in our inbox and our team will resolve it for you quickly. https://www.facebook.com/easypaisa/ You can also contact 
+                     us on Whatsapp: https://wa.me/923411103737?text=Hi""", """Thank you for reaching out to us! 😊, We apologise for the 
+                     inconvenience. Kindly let us know the details of your problem with screenshots in our inbox and our team will resolve it 
+                     right away. www.facebook.com/easypaisa/ 💚 You can also contact us on Whatsapp: https://wa.me/923411103737?text=Hi""", 
+                     """We apologise for the inconvenience and would like to know about any issue you have faced to improve our services. 
+                     We'd be grateful if you could kindly share details with us on our Whatsapp: wa.me/923411103737?text=Hi""","""We apologize 
+                     for any inconvenience you've experienced. 💔 Please provide us with more information about your issue via our inbox, and 
+                     we'll work swiftly to resolve it for you. You can also reach out to us on WhatsApp for immediate assistance: 
+                     https://wa.me/923411103737?text=Hi""", """We understand your frustration and want to ensure your experience is seamless. 💼
+                       Kindly send us the details of your concern through our inbox, and our dedicated team will address it promptly. 
+                       Additionally, you can connect with us on WhatsApp for real-time support: https://wa.me/923411103737?text=Hi"""]
+
+happyComments = ["""We're so glad that you shared your positive experience here. Keep using easypaisa! ✨ For any info & support, feel free to 
+                 reach out to us on Whatsapp: https://wa.me/923411103737?text=Hi 💚""","""We're so happy to have customers like you. 
+                 Thank you for your appreciation. Keep using easypaisa! For any info & support, feel free to reach out to us on 
+                 Whatsapp: https://wa.me/923411103737?text=Hi 💚""","""We're always here for you. Keep using easypaisa! ✨ For any info & support, 
+                 feel free to reach out to us on Whatsapp: https://wa.me/923411103737?text=Hi 💚""","""Thank you for your valuable feedback. Keep 
+                 using easypaisa to make your life easy every day! 🤙 For any info & support, feel free to reach out to us on 
+                 Whatsapp: https://wa.me/923411103737?text=Hi 💚""","""Thank you for rating us the best. Keep using easypaisa! ✨ For any info & 
+                 support, feel free to reach out to us on Whatsapp: https://wa.me/923411103737?text=Hi 💚""","""we are grateful for your support. 
+                 Keep using easypaisa! ✨ For any info & support, feel free to reach out to us on Whatsapp: https://wa.me/923411103737?text=Hi 
+                 💚","Your appreciation means a lot to us. Keep using easypaisa! ✨ For any info & support, feel free to reach out to us on 
+                 Whatsapp: https://wa.me/923411103737?text=Hi 💚""","""We're so glad to know about your positive experience. Keep using easypaisa
+                   to make your life easy every day! 🤙 For any info & support, feel free to reach out to us on 
+                   Whatsapp: https://wa.me/923411103737?text=Hi 💚""","""Feedbacks like yours keep us going! Thank you for acknowledging us. Keep 
+                   using easypaisa. ✨ For any info & support, feel free to reach out to us on Whatsapp: https://wa.me/923411103737?text=Hi 
+                   💚""","""Your satisfaction is our utmost priority. We are glad to make your life easy. Keep using easypaisa! ✨ For any info & 
+                   support, feel free to reach out to us on Whatsapp: https://wa.me/923411103737?text=Hi 💚"""]
--- response.py
+++ response.py
@@ -1,5 +1,6 @@
 import pickle
 import random
+from dictionary import negativeWordsInRoman, apologiseComments, happyComments, greetingsList, highRating, lowRating
 
 
 def getModelandVector():
@@ -19,11 +20,8 @@ def getModelandVector():
 
 def generateResponse(name, rating, comment, predictedRating):
   
-  greetingsList = ["Hey", "Dear","Hello"]
-  highRating = "Thank you for providing us with a high rating. We apologize for any inconvenience and strive to make your experience smoother. 💚 Please share the details of your issue with us through our inbox, and our team will promptly address and resolve it for you. https://www.facebook.com/easypaisa/ 😊"
-  lowRating = "We're delighted that you shared your positive experience. Please consider giving us a higher star rating. Keep using easypaisa! ✨ For any info & support, feel free to reach out to us on Whatsapp: https://wa.me/923411103737?text=Hi 💚"
-  apologiseComments = ["We are sorry for the inconvenience and would like to make your experience easy. 💚 Kindly share the details of your issue in our inbox and our team will resolve it for you quickly. https://www.facebook.com/easypaisa/ You can also contact us on Whatsapp: https://wa.me/923411103737?text=Hi Thank you for reaching out to us! 😊","We apologise for the inconvenience. Kindly let us know the details of your problem with screenshots in our inbox and our team will resolve it right away. www.facebook.com/easypaisa/ 💚 You can also contact us on Whatsapp: https://wa.me/923411103737?text=Hi", "We apologise for the inconvenience and would like to know about any issue you have faced to improve our services. We'd be grateful if you could kindly share details with us on our Whatsapp: wa.me/923411103737?text=Hi"]
-  happyComments = ["We're so glad that you shared your positive experience here. Keep using easypaisa! ✨ For any info & support, feel free to reach out to us on Whatsapp: https://wa.me/923411103737?text=Hi 💚","We're so happy to have customers like you. Thank you for your appreciation. Keep using easypaisa! For any info & support, feel free to reach out to us on Whatsapp: https://wa.me/923411103737?text=Hi 💚","We're always here for you. Keep using easypaisa! ✨ For any info & support, feel free to reach out to us on Whatsapp: https://wa.me/923411103737?text=Hi 💚","Thank you for your valuable feedback. Keep using easypaisa to make your life easy every day! 🤙 For any info & support, feel free to reach out to us on Whatsapp: https://wa.me/923411103737?text=Hi 💚","Thank you for rating us the best. Keep using easypaisa! ✨ For any info & support, feel free to reach out to us on Whatsapp: https://wa.me/923411103737?text=Hi 💚","we are grateful for your support. Keep using easypaisa! ✨ For any info & support, feel free to reach out to us on Whatsapp: https://wa.me/923411103737?text=Hi 💚","Your appreciation means a lot to us. Keep using easypaisa! ✨ For any info & support, feel free to reach out to us on Whatsapp: https://wa.me/923411103737?text=Hi 💚","We're so glad to know about your positive experience. Keep using easypaisa to make your life easy every day! 🤙 For any info & support, feel free to reach out to us on Whatsapp: https://wa.me/923411103737?text=Hi 💚","Feedbacks like yours keep us going! Thank you for acknowledging us. Keep using easypaisa. ✨ For any info & support, feel free to reach out to us on Whatsapp: https://wa.me/923411103737?text=Hi 💚","Your satisfaction is our utmost priority. We are glad to make your life easy. Keep using easypaisa! ✨ For any info & support, feel free to reach out to us on Whatsapp: https://wa.me/923411103737?text=Hi 💚"]
+  
+  
 
   if(predictedRating == 1.0):
     if(rating == 1 or rating == 2 or rating == 3):
@@ -32,7 +30,7 @@ def generateResponse(name, rating, comment, predictedRating):
       finalResponse = greetings + " " + name + " , " +  response
       return finalResponse
     elif(rating == 4 or rating == 5):
-      response = highRating
+      response = random.choice(highRating)
       greetings = random.choice(greetingsList)
       finalResponse = greetings + " " + name + " , " +  response
       return finalResponse
@@ -44,7 +42,7 @@ def generateResponse(name, rating, comment, predictedRating):
       finalResponse = greetings + " " + name + " , " +  response
       return finalResponse
     elif(rating == 4 or rating == 5):
-      response = highRating
+      response = random.choice(highRating)
       greetings = random.choice(greetingsList)
       finalResponse = greetings + " " + name + " , " +  response
       return finalResponse
@@ -56,14 +54,14 @@ def generateResponse(name, rating, comment, predictedRating):
       finalResponse = greetings + " " + name + " , " +  response
       return finalResponse
     elif(rating == 4 or rating == 5):
-      response = highRating
+      response = random.choice(highRating)
       greetings = random.choice(greetingsList)
       finalResponse = greetings + " " + name + " , " +  response
       return finalResponse
 
   elif(predictedRating == 4.0):
     if(rating == 1 or rating == 2 or rating == 3):
-      response = lowRating
+      response = random.choice(lowRating)
       greetings = random.choice(greetingsList)
       finalResponse = greetings + " " + name + " , " +  response
       return finalResponse
@@ -75,7 +73,7 @@ def generateResponse(name, rating, comment, predictedRating):
 
   elif(predictedRating == 5.0):
     if(rating == 1 or rating == 2 or rating == 3):
-      response = lowRating
+      response = random.choice(lowRating)
       greetings = random.choice(greetingsList)
       finalResponse = greetings + " " + name + " , " +  response
       return finalResponse
@@ -85,14 +83,29 @@ def generateResponse(name, rating, comment, predictedRating):
       finalResponse = greetings + " " + name + " , " +  response
       return finalResponse
 
+def preprocessComment(comment):
+    comment = comment.lower()
+    for word in negativeWordsInRoman:
+      #print(word)
+      if word in comment:
+          comment = comment.replace(word, "bakwas")
+          return comment
+    return comment
+
 
 def getResponse(name, rating, comment):
     model, vectorizer = getModelandVector()
+    processedComment = preprocessComment(comment)
 
-    new_query = [comment]
+    print(processedComment)
+
+    new_query = [processedComment]
     new_query_tfidf = vectorizer.transform(new_query)
     predicted_star_rating = model.predict(new_query_tfidf)
 
     predictedRating = float(predicted_star_rating[0])
-    finalResponse = generateResponse(name, rating, comment, predictedRating)
+    finalResponse = generateResponse(name, rating, processedComment, predictedRating)
     return finalResponse
+
+
+#print(preprocessComment("app is not good"))
