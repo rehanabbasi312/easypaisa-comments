97d5d5240f182e695af869088c62d390cea5bcf0
unknown <rehan.naveed@zigron.com>
2024-05-20 08:11:25 +0500

    UPDATE CODE

--- app.py
+++ app.py
@@ -9,21 +9,22 @@ loading_messages = []
 
 def email_checker():
     global loading_messages
-    i=0
-    while True:
-        loading_messages.append(f"{i} Loading...")
-        check_email()  # Perform email checking here
-        time.sleep(10)  # Check email every 2 minutes
-        i+=1
+    
+    loading_messages.append(f" Loading...")
+    check_email()  # Perform email checking here
+    time.sleep(10)  # Check email every 2 minutes
+        
 
 @app.route('/')
 def index():
+    loading_messages.append(f"Loading...")
+    print("Loading ... ")
+    check_email()
     return render_template('index.html', loading_messages=loading_messages)
 
 if __name__ == "__main__":
     # Start a separate thread for email checking
-    email_thread = threading.Thread(target=email_checker)
-    email_thread.daemon = True
-    email_thread.start()
-    
+    #email_thread = threading.Thread(target=email_checker)
+    #email_thread.daemon = True
+    #email_thread.start()
     app.run(debug=True)
